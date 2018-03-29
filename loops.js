2c4611658addb24d2968bf25e48459702751478a
Michael R. McGown <mrmcgown@gmail.com>
2018-03-29 20:00:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
 for (var i = 0; i < 25; i++) {
-  if(array.length > 1) {
+  if(array.length === 1) {
     console.log("I am ${i} strange loop.");
 } else {
   console.log("I am ${i} strange loops.");
