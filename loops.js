7ca6ecb64e09ffc9c763d75ef9274da28ab76e13
Michael R. McGown <mrmcgown@gmail.com>
2018-03-29 20:09:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ for (var i = 0; i < 25; i++) {
   if(i === 1) {
     array.push("I am ${i} strange loop.");
 } else {
-  console.log("I am ${i} strange loops.");
+    array.push("I am ${i} strange loops.");
 }
 }
   return array;
