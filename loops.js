68573b1cf7695a023c1c8a04894ab8a6ab36f142
Michael R. McGown <mrmcgown@gmail.com>
2018-03-29 19:32:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
-for ([];[];[]) {
-  []
+function forLoop() {
+for (var i = 0; i < 25; i++) {
+  if(i === 1) {
+    console.log("I am ${i} strange loop.")
+} else {
+  console.log("I am ${i} strange loops.")
+}
 }
